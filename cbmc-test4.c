abc61ee3025589674e03b73808823c31a57c0fc9
Saiyajin22 <tothzoltan0713@gmail.com>
2024-04-28 21:54:04 +0200

    updates

--- cbmc-test4.c
+++ cbmc-test4.c
@@ -5,22 +5,62 @@ int plusTen(int num) {
     return num += 10;
 }
 
+unsigned long long factorial(unsigned int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+unsigned int sha_h[8] = {0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19};
+
+void sha_initstate(unsigned int *state)
+{
+    int n;
+
+    for (n = 0; n < 8; n++)
+    {
+        *state = sha_h[n];
+        state++;
+    }
+}
+
 int main()
 {
     unsigned int x = 0;
+    unsigned int state[8];
+    sha_initstate((unsigned int *)&state);
 
     #ifdef CBMC
         x = nondet_uint();
-        __CPROVER_assume(x > 0 && x < 1000);
+        __CPROVER_assume(x > 0 && x <= 2000);
     #endif
 
-    x = plusTen(x);
-    if (x > 40) {
-        assert(0);
-    }
-    printf("x: %d\n", x);
+    // printf("x: %d\n", x);
+    state[7] = x;
+    printf("state[7]: %d\n", state[7]);
 
+    unsigned long long result = factorial(200000);
 
-    // assert(0);
+#ifdef CBMC
+        // __CPROVER_assume(
+        //     (unsigned char)((state[7] >> 0) & 0xff) == 0x00 &&
+        //     (unsigned char)((state[7] >> 8) & 0xff) == 0x00 &&
+        //     (unsigned char)((state[7] >> 16) & 0xff) == 0x00);
+        __CPROVER_assume(state[7] > 1992);
+    #endif
+
+    // x = plusTen(x);
+    // if (x > 2008)
+    // {
+    //     assert(0);
+    // }
+
+    assert(0);
     return 0;
 }
